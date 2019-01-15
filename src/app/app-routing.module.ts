40dcfb8fd8c98e71c0c9b8a710c517da84920cd7
SabikAbtahee <bsse0829@iit.du.ac.bd>
2019-01-15 18:19:19 +0600

    Firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { PersonComponent } from './person/person.component';
+import { CreatePersonComponent } from './create-person/create-person.component';
+import { ViewPersonComponent } from './view-person/view-person.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path     : 'user',
+    component: UserComponent
+  },
+  {
+    path     : 'person',
+    component: PersonComponent
+  },
+  {
+    path     : 'person/create',
+    component: CreatePersonComponent
+  },
+  {
+    path     : 'person/create/:id',
+    component: CreatePersonComponent
+  },
+  {
+    path     : 'person/view',
+    component: ViewPersonComponent
+  },
+ 
+  {
+    path      : '',
+    redirectTo: '/user',
+    pathMatch : 'full'
+  },
+  {
+    path      : 'imrulVai',
+    
+    component: MainContentComponent
+  },
+  
+  { path: '**', 
+  component: NotFoundComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,22 @@
 
 export const environment = {
   production: false,
+  // firebase:{
+  //   apiKey: "AIzaSyCxJTI5qXgyCGDyWeA8NbiVNJr77zBdB5E",
+  //   authDomain: "angular1-74ac7.firebaseapp.com",
+  //   databaseURL: "https://angular1-74ac7.firebaseio.com",
+  //   projectId: "angular1-74ac7",
+  //   storageBucket: "angular1-74ac7.appspot.com",
+  //   messagingSenderId: "1081153988091"
+  // }
+
   firebase:{
-    apiKey: "AIzaSyCxJTI5qXgyCGDyWeA8NbiVNJr77zBdB5E",
-    authDomain: "angular1-74ac7.firebaseapp.com",
-    databaseURL: "https://angular1-74ac7.firebaseio.com",
-    projectId: "angular1-74ac7",
-    storageBucket: "angular1-74ac7.appspot.com",
-    messagingSenderId: "1081153988091"
+    apiKey: "AIzaSyCgUy_-ozh-DZMYHHPr87a2N0-Stwanxs8",
+    authDomain: "angulartest1-deacb.firebaseapp.com",
+    databaseURL: "https://angulartest1-deacb.firebaseio.com",
+    projectId: "angulartest1-deacb",
+    storageBucket: "angulartest1-deacb.appspot.com",
+    messagingSenderId: "814295625631"
   }
 };
 
